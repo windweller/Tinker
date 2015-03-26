141dea7f3bd760be0c56f9d293fd109652cccb94
windweller <leo.niecn@gmail.com>
2015-03-26 07:15:30 -0400

    parser adding

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -1,11 +1,19 @@
 package parser
 
+import files.DataContainer
 import parser.implementations.ParserImpl
+import parser.processing.Processing
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Parser has a general meaning not just
  * sentence parsing, but tregex matching as well
  */
-abstract class Parser extends ParserImpl {
+abstract class Parser() extends ParserImpl with Processing {
+
+  val actionStream: ArrayBuffer[(DataContainer) => DataContainer] = ArrayBuffer.empty[(DataContainer) => DataContainer]
+
+
 
 }
