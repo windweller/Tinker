2078ecfc10f14572ea9fefc5503d90a85736da20
windweller <leo.niecn@gmail.com>
2015-04-30 00:41:12 -0400

    Add Sentiment analysis

--- src/main/scala/newFiles/operations/FileOp.scala
+++ src/main/scala/newFiles/operations/FileOp.scala
@@ -3,6 +3,9 @@ package newFiles.operations
 import newFiles.DataContainer
 import newProcessing.Operation
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 
 /**
  * Created by anie on 4/18/2015.
@@ -12,6 +15,27 @@ trait FileOp extends DataContainer with Operation {
   def combine(data2: DataContainer): DataContainer with FileOp = {
     iterators ++= data2.iterators
     this
+  }
+
+  def averageByGroup(saveLoc: String): Unit = {
+    val it = iterators.head
+    val result = ArrayBuffer.empty[mutable.HashMap[String, Double]]
+
+    it.foreach { group =>
+      val itr = group._2
+      val sumForGroup = mutable.HashMap.empty[String, Double]
+      itr.foreach {row =>
+        val it = row.iterator
+        it.foreach(pair =>
+          if (sumForGroup.get(pair._1).isEmpty)
+            sumForGroup += (pair._1 -> pair._2.toDouble)
+          else
+            sumForGroup.update(pair._1, sumForGroup(pair._1) + pair._2.toDouble)
+        )
+      }
+
+      result += sumForGroup
+    }
   }
 
 
---
+++ src/main/scala/utils/collections/MapEnhancement.scala
@@ -0,0 +1,8 @@
+package utils.collections
+
+/**
+ * Created by Aimingnie on 4/29/15.
+ */
+class MapEnhancement {
+
+}
