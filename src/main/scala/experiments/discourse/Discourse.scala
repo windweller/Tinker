5b5d423612477acf76fdac951999dd4c414a1984
windweller <leo.niecn@gmail.com>
2017-07-21 00:16:26 -0700

    ready for deparse annotator mode

--- src/main/scala/experiments/discourse/Discourse.scala
+++ src/main/scala/experiments/discourse/Discourse.scala
@@ -1,5 +1,9 @@
 package experiments
 
+import java.util.Properties
+
+import edu.stanford.nlp.ling.{CoreAnnotations, CoreLabel}
+import edu.stanford.nlp.ling.CoreAnnotations.{TextAnnotation, TokensAnnotation}
 import files.DataContainer
 import files.filetypes.input.{CSV, Tab}
 import files.structure.DataStructure
@@ -7,12 +11,38 @@ import nlp.future.FutureRules._
 import nlp.preprocess.tokenization.Tokenizer
 import nlp.preprocess.tokenization.impl.Stanford
 import utils.ParameterCallToOption.Implicits._
-
+import edu.stanford.nlp.pipeline.StanfordCoreNLPServer
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations
 import edu.stanford.nlp.simple._
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import edu.stanford.nlp.pipeline._
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.EnhancedDependenciesAnnotation
+import edu.stanford.nlp.util.CoreMap
+
+import scala.collection.JavaConversions._
+
+
 
 object Discourse extends App {
 
-  val sent = new Sentence("Lucy is in the sky with diamonds.")
-  println(sent.incomingDependencyLabels())
+//  val sent = new Sentence("Lucy is in the sky with diamonds.")
+//  println(sent.incomingDependencyLabels())
 
+  val props = new Properties()
+  props.setProperty("annotators", "tokenize,ssplit,pos,depparse")
+
+
+  val pipeline = new StanfordCoreNLP(props)
+  val document = new Annotation("Lucy is in the sky with diamonds.")
+
+  pipeline.annotate(document)
+
+
+  val sentences = document.get(classOf[CoreAnnotations.SentencesAnnotation])
+
+  for (sentence: CoreMap <- sentences) { // traversing the words in the current sentence
+    // this is the Stanford dependency graph of the current sentence
+    val dependencies = sentence.get(classOf[EnhancedDependenciesAnnotation])
+    println(dependencies)
+  }
 }
--- src/main/scala/nlp/parser/DependencyParser.scala
+++ src/main/scala/nlp/parser/DependencyParser.scala
@@ -4,10 +4,9 @@ import edu.stanford.nlp.parser.nndep.DependencyParser
 import edu.stanford.nlp.trees.Tree
 import edu.stanford.nlp.simple._
 
-
 class DependencyParser {
   def parse(text: String) {
     val sent = new Sentence(text)
-    return sent.incomingDependencyLabels()
+    sent.incomingDependencyLabels()
   }
 }
